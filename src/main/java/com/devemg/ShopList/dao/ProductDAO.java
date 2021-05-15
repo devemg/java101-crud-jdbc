9a007c0f9ffd22afab3a2ec685324933dbf79ece
devemg <garciam.emm@gmail.com>
2021-05-15 17:00:07 -0600

    Update and delete products

--- src/main/java/com/devemg/ShopList/dao/ProductDAO.java
+++ src/main/java/com/devemg/ShopList/dao/ProductDAO.java
@@ -6,9 +6,12 @@ import java.sql.*;
 import java.util.*;
 
 public class ProductDAO {
-    // Definir sentencias
-   private static final String SQL_SELECT = "SELECT id, name, budget, quantity, image, description FROM product";
-   private static final String SQL_INSERT = "INSERT INTO product(name, budget, quantity, image, description) VALUES (?,?,?,?,?)";
+    // Define SQL sentences
+    private static final String SQL_SELECT = "SELECT id, name, budget, quantity, image, description FROM product";
+    private static final String SQL_SELECT_ONE = "SELECT id, name, budget, quantity, image, description FROM product WHERE id = ?";
+    private static final String SQL_INSERT = "INSERT INTO product(name, budget, quantity, image, description) VALUES (?,?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE product SET name=?,budget=?,quantity=?,image=?, description=? WHERE id = ?";
+    private static final String SQL_DELETE = "DELETE FROM product WHERE id = ?";
 
    public List<Product> selectAll(){
        Connection conn = null;
@@ -48,6 +51,44 @@ public class ProductDAO {
     return products;
    }
 
+    public Product select(int idProduct){
+        Connection conn = null;
+        PreparedStatement pStatement = null;
+        ResultSet rs = null;
+        Product product = null;
+        try {
+            conn = getConnection();
+            pStatement = conn.prepareStatement(SQL_SELECT_ONE);
+            pStatement.setInt(1,idProduct);
+            rs = pStatement.executeQuery();
+            while (rs.next()){
+                product = new Product(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getDouble("budget"),
+                        rs.getInt("quantity"),
+                        rs.getString("image"),
+                        rs.getString("description")
+                );
+            }
+
+
+        }catch (SQLException ex){
+            ex.printStackTrace(System.err);
+        }
+        finally {
+            try {
+                close(rs);
+                close(pStatement);
+                close(conn);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+        return product;
+    }
+
     public int insert(Product product){
        // vaidate name
         if(product.getName() == null) {
@@ -66,6 +107,59 @@ public class ProductDAO {
             pStatement.setString(5,product.getDescription());
             result = pStatement.executeUpdate();
             
+        }catch (SQLException ex){
+            ex.printStackTrace(System.err);
+        }
+        finally {
+            try {
+                close(pStatement);
+                close(conn);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+        return result;
+    }
+
+    public int update(int idProduct,Product product){
+        Connection conn = null;
+        PreparedStatement pStatement = null;
+        int result = 0;
+        try {
+            conn = getConnection();
+            pStatement = conn.prepareStatement(SQL_UPDATE);
+            pStatement.setString(1,product.getName());
+            pStatement.setDouble(2,product.getBudget());
+            pStatement.setInt(3,product.getQuantity());
+            pStatement.setString(4,product.getImage());
+            pStatement.setString(5,product.getDescription());
+            pStatement.setInt(6,idProduct);
+            result = pStatement.executeUpdate();
+        }catch (SQLException ex){
+            ex.printStackTrace(System.err);
+        }
+        finally {
+            try {
+                close(pStatement);
+                close(conn);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+        return result;
+    }
+
+    public int delete(int idProduct){
+        Connection conn = null;
+        PreparedStatement pStatement = null;
+        int result = 0;
+        try {
+            conn = getConnection();
+            pStatement = conn.prepareStatement(SQL_DELETE);
+            pStatement.setInt(1,idProduct);
+            result = pStatement.executeUpdate();
         }catch (SQLException ex){
             ex.printStackTrace(System.err);
         }
--- src/test/java/TestProducts.java
+++ src/test/java/TestProducts.java
@@ -6,9 +6,17 @@ import java.util.List;
 public class TestProducts {
     public static void main(String[] args) {
         ProductDAO productDao = new ProductDAO();
-        int result = productDao.insert(new Product("Fideos", 3, 10,"url","Paquete de fideos"));
-        System.out.println(result>0?"INSERTADO":"NO INSERTADO");
-        List<Product> products = productDao.selectAll();
-        products.forEach(System.out::println);
+
+       // int result = productDao.insert(new Product("Fideos", 3, 10,"url","Paquete de fideos"));
+        //System.out.println(result>0?"INSERTADO":"NO INSERTADO");
+        //List<Product> products = productDao.selectAll();
+        //products.forEach(System.out::println);
+        Product prod = productDao.select(7);
+        System.out.println(prod);
+        prod.setBudget(15);
+        prod.setDescription("Prueba de actualización");
+        productDao.update(7,prod);
+        prod = productDao.select(7);
+        System.out.println(prod);
     }
 }
